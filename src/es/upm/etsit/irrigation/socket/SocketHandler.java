0c42855c87ae58a8414f098093e3cb04ea7843a2
Josean_Llamas <josean_soy_yo@hotmail.es>
2018-03-18 04:19:28 +0100

    Comunicaciones

--- src/es/upm/etsit/irrigation/socket/SocketHandler.java
+++ src/es/upm/etsit/irrigation/socket/SocketHandler.java
@@ -1,19 +1,52 @@
 package es.upm.etsit.irrigation.socket;
-
-import es.upm.etsit.irrigation.shared.Mode;
+import javax.xml.bind.DatatypeConverter;
 
 public class SocketHandler {
 
   
-  public static Mode askMode() {
+  public static byte[] askMode(String identificador) {
+    String respuesta = Comunicaciones.consultarAlServidor("/ActMd/" + identificador + "/", 1)[0];
+    if(respuesta != null){
+    	if(respuesta.equals("IdFalso")){
+    		return new byte[0];
+    	}
+    	byte[] modoSerializado = DatatypeConverter.parseHexBinary(respuesta);
+    	return modoSerializado;
+    }
     return null;
   }
   
-  public static Integer[] shouldIrrigateNow() {
+  public static Integer[] shouldIrrigateNow(String identificador) {
+    String respuesta = Comunicaciones.consultarAlServidor("/ShReg/" + identificador + "/", 1)[0];
+    if(respuesta != null){
+    	if(respuesta.equals("IdFalso")){
+    		return new Integer[0];
+    	}
+    	Integer[] salida = new Integer[32];
+    	String[] separados = respuesta.substring(1, respuesta.length() - 1).split("/");
+    	for(int j1 = 0; j1 < 32; j1 = j1 + 1){
+    		if(separados[j1].equals("null") == false){
+    			salida[j1] = Integer.parseInt(separados[j1]);
+    		}
+    		else{
+    			salida[j1] = 0;
+    		}
+    	}
+    	return salida;
+    }
     return null;
   }
   
-  public static void sendPortStatus(Boolean[] status) {
-    
+  public static void sendPortStatus(Boolean[] status, String identificador) {
+    String peticion = "/InfRg/" + identificador + "/";
+    for(int j1 = 0; j1 < 32; j1 = j1 + 1){
+    	if(status[j1] != null){
+    		peticion = peticion + Boolean.toString(status[j1]) + "/";
+    	}
+    	else{
+    		peticion = peticion + "null/";
+    	}
+    }
+    Comunicaciones.consultarAlServidor(peticion, 0);
   }
 }
